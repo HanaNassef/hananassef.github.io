455321608ffce1a3b19bbc15574513a28074ca6d
HanaNassef <159064036+HanaNassef@users.noreply.github.com>
2025-08-28 22:58:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,8 @@ window.addEventListener('scroll', () => {
   backToTop.style.display = window.scrollY > 300 ? "block" : "none";
 });
 backToTop.addEventListener('click', () => window.scrollTo({top: 0, behavior: 'smooth'}));
+
+function toggleSkills(skillType) {
+  const list = document.getElementById(skillType);
+  list.style.display = (list.style.display === "block") ? "none" : "block";
+}
